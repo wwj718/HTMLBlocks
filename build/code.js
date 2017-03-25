2439514aa63398311bc6c8a5e7c9b075964946a0
TheBrokenRail <connor24nolan@live.com>
2017-03-25 14:28:49 -0400

    Update code.js

--- build/code.js
+++ build/code.js
@@ -27,11 +27,30 @@ window.onload = function () {
         var xml = window.ScratchBlocks.Xml.workspaceToDom(window.workspace);
         project.blocks = window.ScratchBlocks.Xml.domToPrettyText(xml);
         var json = JSON.stringify(project, null, 4);
-
         var hiddenElement = document.createElement('a');
         hiddenElement.href = 'data:attachment/text,' + encodeURI(json);
         hiddenElement.target = '_blank';
         hiddenElement.download = name + '.json';
         hiddenElement.click();
+    };
+    
+    document.getElementById('load').onclick = function () {
+        var input = document.createElement('INPUT');
+        input.type = 'file';
+        input.onchange = function () {
+            if (input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function(){
+                    var text = reader.result;
+                    workspace.clear();
+                    var project = JSON.parse(text);
+                    document.getElementById('name').value = project.name;
+                    var xml = window.ScratchBlocks.Xml.textToDom(project.blocks);
+                    window.ScratchBlocks.Xml.domToWorkspace(xml, workspace);
+                };
+                reader.readAsText(input.files[0]);
+            }
+        };
+        input.click();
     };
 };

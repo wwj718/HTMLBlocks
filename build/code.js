30c9c533db1234efb34cfbf40c84ec6dec9215eb
TheBrokenRail <connor24nolan@live.com>
2017-03-25 11:39:36 -0400

    Upload

---
+++ build/code.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    var workspace = window.ScratchBlocks.inject('blocks', {
+        media: './media/',
+        zoom: {
+            controls: true,
+            wheel: true,
+            startScale: 0.75
+        },
+        colours: {
+            fieldShadow: 'rgba(255, 255, 255, 0.3)',
+            dragShadowOpacity: 0.6
+        }
+    });
+    window.workspace = workspace;
+};

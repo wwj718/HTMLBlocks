f17cafe1689c003a32476791bef72768253cff65
TheBrokenRail <connor24nolan@live.com>
2017-03-25 12:56:03 -0400

    Update code.js

--- build/code.js
+++ build/code.js
@@ -17,3 +17,20 @@ window.onload = function () {
     window.ScratchBlocks.Xml.domToWorkspace(document.getElementById('workspace'), workspace);
     workspace.scrollCenter();
 };
+
+document.getElementById('save').onclick = function () {
+    var name = document.getElementById('name').value;
+    if (!name || name === "") {
+        name = "Untitled";
+    }
+    var project = {};
+    project.name = name;
+    var xml = window.ScratchBlocks.Xml.workspaceToDom(window.workspace);
+    project.blocks = window.ScratchBlocks.Xml.domToPrettyText(xml);
+    var json = JSON.toString(project, null, 4);
+    var hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:attachment/text,' + encodeURI(json);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = name + '.json';
+    hiddenElement.click();
+};

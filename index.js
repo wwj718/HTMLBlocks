ce57725625d9e9bfa020c5ddfc9b0e616ecedc3e
TheBrokenRail <connor24nolan@live.com>
2017-04-21 17:31:32 -0400

    Fix 3

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.Blockly = require('scratch-blocks');
 window.ScratchBlocks = window.Blockly;
-window.ScratchBlocks.JavaScript = require('scratch-blocks/javascript_compressed')(Blockly);
+window.ScratchBlocks.JavaScript = require('scratch-blocks/javascript_compressed');

0a1194ad7d852967712c6728a451323ba243ed54
TheBrokenRail <connor24nolan@live.com>
2017-04-28 17:33:39 -0400

    Create css_blocks.js

---
+++ build/css_blocks.js
@@ -0,0 +1,31 @@
+var ScratchBlocks = window.ScratchBlocks;
+ScratchBlocks.Categories.css = {};
+
+ScratchBlocks.Blocks['css_selector'] = {
+    init: function() {
+        this.jsonInit({
+            "id": "css_selector",
+            "message0": "selector %1",
+            "message1": "%1",
+            "args0": [
+                {
+                    "type": "input_value",
+                    "name": "SELECTOR"
+                }
+            ],
+            "args1": [
+                {
+                    "type": "input_statement",
+                    "name": "SUBSTACK"
+                }
+            ],
+            "inputsInline": true,
+            "previousStatement": null,
+            "nextStatement": null,
+            "category": ScratchBlocks.Categories.css,
+            "colour": ScratchBlocks.Colours.looks.primary,
+            "colourSecondary": ScratchBlocks.Colours.looks.secondary,
+            "colourTertiary": ScratchBlocks.Colours.looks.tertiary
+        });
+    }
+};

f608070070cd621a4d3e2f3b0fdce25daa134df8
TheBrokenRail <connor24nolan@live.com>
2017-04-28 18:57:05 -0400

    Update css_blocks.js

--- build/css_blocks.js
+++ build/css_blocks.js
@@ -88,3 +88,68 @@ ScratchBlocks.JavaScript['css_selector'] = function (block) {
         ScratchBlocks.JavaScript.statementToCode(block, 'SUBSTACK') + 
         "}'));";
 };
+
+ScratchBlocks.Blocks['css_attribute'] = {
+    init: function () {
+        this.jsonInit({
+            "id": "css_attribute",
+            "message0": "set %1 to %2",
+            "args0": [
+                {
+                    "type": "input_value",
+                    "name": "ATTRIBUTE"
+                },
+                {
+                    "type": "input_value",
+                    "name": "TO"
+                }
+            ],
+            "inputsInline": true,
+            "previousStatement": null,
+            "nextStatement": null,
+            "category": ScratchBlocks.Categories.css,
+            "colour": ScratchBlocks.Colours.looks.primary,
+            "colourSecondary": ScratchBlocks.Colours.looks.secondary,
+            "colourTertiary": ScratchBlocks.Colours.looks.tertiary
+        });
+    },
+    onchange: function () {
+        function getTop(block) {
+            if (block.getParent()) {
+                return getTop(block.getParent());
+            }
+            return block;
+        }
+        var realTop = getTop(this);
+        var top = this.getParent() || this;
+        if (!top.previousConnection && top.type === "css_selector" && realTop.type === "css") {
+            this.setDisabled(false);
+            if (!this.isInsertionMarker()) this.setColour(
+                ScratchBlocks.Colours.looks.primary,
+                ScratchBlocks.Colours.looks.secondary,
+                ScratchBlocks.Colours.looks.tertiary
+            );
+        } else if (realTop.previousConnection) {
+            this.setDisabled(false);
+            if (!this.isInsertionMarker()) this.setColour(
+                ScratchBlocks.Colours.looks.primary,
+                ScratchBlocks.Colours.looks.secondary,
+                ScratchBlocks.Colours.looks.tertiary
+            );
+        } else {
+            this.setDisabled(true);
+            if (!this.isInsertionMarker()) this.setColour(
+                "#CCCCAE",
+                "#CCCCAE",
+                "#CCCCAE"
+            );
+        }
+    }
+};
+
+ScratchBlocks.JavaScript['css_attribute'] = function (block) {
+    return ScratchBlocks.JavaScript.valueToCode(block, 'ATTRIBUTE') + 
+        ": " + 
+        ScratchBlocks.JavaScript.valueToCode(block, 'TO') + 
+        ";";
+};

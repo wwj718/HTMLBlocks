4ce152199c503dd96958483dec663de2ed3470ce
TheBrokenRail <connor24nolan@live.com>
2017-04-29 13:41:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var ScratchBlocks = require('scratch-blocks');
+window.beautify = require('js-beautify');
 window.ScratchBlocks = ScratchBlocks;
 window.Blockly = ScratchBlocks;
 require('scratch-blocks/javascript_compressed');

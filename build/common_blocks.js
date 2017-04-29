430f1c08bcd841396995651f9a3d86abd40a2291
TheBrokenRail <connor24nolan@live.com>
2017-04-29 13:19:36 -0400

    Update common_blocks.js

--- build/common_blocks.js
+++ build/common_blocks.js
@@ -1,5 +1,7 @@
 var ScratchBlocks = window.ScratchBlocks;
 
+ScratchBlocks.JavaScript.INDENT = "";
+
 ScratchBlocks.JavaScript["text"] = function (block) {
     return [block.getFieldValue('TEXT')];
 };

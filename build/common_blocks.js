56225975261e8dcf888979ae7a1c2bcbad5fb63f
TheBrokenRail <connor24nolan@live.com>
2017-05-14 14:07:52 -0400

    Update common_blocks.js

--- build/common_blocks.js
+++ build/common_blocks.js
@@ -5,3 +5,25 @@ ScratchBlocks.JavaScript.INDENT = "";
 ScratchBlocks.JavaScript["text"] = function (block) {
     return [block.getFieldValue('TEXT')];
 };
+
+var newNameBlock = function (internalName, attributeName) {
+    var ScratchBlocks = window.ScratchBlocks;
+    ScratchBlocks.Blocks[internalName] = {
+        init: function() {
+            this.jsonInit({
+                "id": internalName,
+                "message0": attributeName,
+                "inputsInline": true,
+                "category": ScratchBlocks.Categories.html,
+                "colour": ScratchBlocks.Colours.control.primary,
+                "colourSecondary": ScratchBlocks.Colours.control.secondary,
+                "colourTertiary": ScratchBlocks.Colours.control.tertiary,
+                "outputShape": ScratchBlocks.OUTPUT_SHAPE_ROUND,
+                "output": "String"
+            });
+        }
+    };
+    ScratchBlocks.JavaScript[internalName] = function (block) {
+        return [attributeName];
+    };
+};

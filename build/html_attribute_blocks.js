c0f5a28c626063d76de9478947f3774da2156b76
TheBrokenRail <connor24nolan@live.com>
2017-05-14 13:52:20 -0400

    Update html_attribute_blocks.js

--- build/html_attribute_blocks.js
+++ build/html_attribute_blocks.js
@@ -1,1 +1,35 @@
+var newHtmlAttributeBlock = function (internalName, displayName, attributeName) {
+    var ScratchBlocks = window.ScratchBlocks;
+    ScratchBlocks.Blocks[internalName] = {
+        init: function() {
+            this.jsonInit({
+                "id": internalName,
+                "message0": displayName,
+                "inputsInline": true,
+                "category": ScratchBlocks.Categories.html,
+                "colour": ScratchBlocks.Colours.control.primary,
+                "colourSecondary": ScratchBlocks.Colours.control.secondary,
+                "colourTertiary": ScratchBlocks.Colours.control.tertiary,
+                "outputShape": ScratchBlocks.OUTPUT_SHAPE_ROUND
+            });
+        },
+        onchange: function () {
+            var top = this.getParent();
+            if (top && top.disabled) {
+                this.setDisabled(true);
+                if (!this.isInsertionMarker()) this.setOpacity(0.45);
+            } else if (top && top.type === "html_attribute") {
+                this.setDisabled(false);
+                if (!this.isInsertionMarker()) this.setOpacity(1);
+            } else if (!top) {
+                this.setDisabled(false);
+                if (!this.isInsertionMarker()) this.setOpacity(1);
+            } else {
+                this.setDisabled(true);
+                if (!this.isInsertionMarker()) this.setOpacity(0.45);
+            }
+        }
+    };
+};
 
+newHtmlAttributeBlock("html_attribute_href", "href", "href");

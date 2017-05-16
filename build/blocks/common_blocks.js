91936bd4c78c1e8313a81ff0a797435513adc176
TheBrokenRail <connor24nolan@live.com>
2017-05-16 17:24:01 -0400

    Update common_blocks.js

--- build/blocks/common_blocks.js
+++ build/blocks/common_blocks.js
@@ -39,17 +39,17 @@ var newNameBlock = function (internalName, attributeName, category, block) {
             });
         },
         onchange: function () {
-            if (this.getParent() && this.getParent().disabled) {
-                this.setDisabled(true);
-            } else {
-                this.setDisabled(false);
-            }
             if (this.getParent() && this.getParent().type === block) {
                 this.setDisabled(true);
                 this.getParent().setDisabled(true);
             } else {
                 this.setDisabled(false);
                 this.getParent().setDisabled(false);
+            }
+            if (this.getParent() && this.getParent().disabled) {
+                this.setDisabled(true);
+            } else {
+                this.setDisabled(false);
             }
             if (this.disabled) {
                 if (!this.isInsertionMarker()) this.setOpacity(0.45);

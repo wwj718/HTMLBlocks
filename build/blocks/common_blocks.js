6948b842aff7e27b8f64008db7e46136f415d45a
TheBrokenRail <connor24nolan@live.com>
2017-05-16 20:45:56 -0400

    Update common_blocks.js

--- build/blocks/common_blocks.js
+++ build/blocks/common_blocks.js
@@ -43,11 +43,12 @@ var newNameBlock = function (internalName, attributeName, category, block) {
             if (this.getParent() && this.getParent().disabled) {
                 this.setDisabled(true);
             } else {
-                if (this.getParent() && this.getParent().type !== block) {
-                    this.setDisabled(true);
-                } else {
-                    this.setDisabled(false);
-                }
+                this.setDisabled(false);
+            }
+            if (this.getParent() && this.getParent().type !== block) {
+                this.setDisabled(true);
+            } else {
+                this.setDisabled(false);
             }
             if (this.disabled) {
                 if (!this.isInsertionMarker()) this.setOpacity(0.45);

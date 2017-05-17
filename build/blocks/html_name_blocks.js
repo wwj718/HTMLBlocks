f752d3a3c28ca43354958ea640100653e3734e98
TheBrokenRail <connor24nolan@live.com>
2017-05-17 18:12:15 -0400

    Update html_name_blocks.js

--- build/blocks/html_name_blocks.js
+++ build/blocks/html_name_blocks.js
@@ -1,2 +1,9 @@
+// Attributes
 newNameBlock("html_name_href", "href", "html", "html_attribute", "ATTRIBUTE");
+newNameBlock("html_name_src", "src", "html", "html_attribute", "ATTRIBUTE");
+newNameBlock("html_name_id", "id", "html", "html_attribute", "ATTRIBUTE");
+
+// Elements
 newNameBlock("html_name_p", "p", "html", "html_element", "ELEMENT");
+newNameBlock("html_name_a", "a", "html", "html_element", "ELEMENT");
+newNameBlock("html_name_img", "img", "html", "html_element", "ELEMENT");
